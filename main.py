419d9c65942d309d31150d05eb8c1b644d4676c8
sumzali083 <alisummer066@icloud.com>
2026-04-19 14:09:39 +0100

    Refactor code to loop through a list of ETFs for historical price data retrieval

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import yfinance as yf
 
-spy = yf.Ticker("SPY")
-spy_price_data = spy.history(period="3mo")
-print(spy_price_data)
 
-qqq = yf.Ticker("QQQ")
-qqq_price_data = qqq.history(period="3mo")
-print(qqq_price_data)
-
-ivv = yf.Ticker("IVV")
-ivv_price_data = ivv.history(period="3mo")
-print(ivv_price_data)
-
-agg = yf.Ticker("AGG")
-agg_price_data = agg.history(period="3mo")
-print(agg_price_data)
-
-gld = yf.Ticker("GLD")
-gld_price_data = gld.history(period="3mo")
-print(gld_price_data)
+list_of_etfs = ["SPY", "QQQ", "IVV", "AGG", "GLD"]
+for x in list_of_etfs:
+    ticker = yf.Ticker(x)
+    price_data = ticker.history(period="3mo")
+    print(price_data)
