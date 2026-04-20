e6fc4e79d3f19619ed86f5e3a2258eb07b984806
sumzali083 <alisummer066@icloud.com>
2026-04-20 13:37:07 +0100

    Refactor main.py to establish database connection and insert historical price data for ETFs

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import yfinance as yf
+import sqlite3
+
+conn = sqlite3.connect("etf_monitor.db")
+cursor = conn.cursor()
 
 
 list_of_etfs = ["SPY", "QQQ", "IVV", "AGG", "GLD"]
@@ -6,3 +10,16 @@ for x in list_of_etfs:
     ticker = yf.Ticker(x)
     price_data = ticker.history(period="3mo")
     print(price_data)
+
+for date, row in price_data.iterrows():
+    cursor.execute("""
+        INSERT INTO daily_prices (ticker, date, open, close, high, low, volume)
+        VALUES (?, ?, ?, ?, ?, ?, ?)
+    """,(x, str(date.date()), row["Open"], 
+              row["Close"], row["High"], row["Low"], 
+              int(row["Volume"])))
+conn.commit()
+conn.close()
+print("Data inserted into database!")
+
+
